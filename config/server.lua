48bac7fa23f814c5b82f744cb59a15cb7e81c444
OGReaper <102786953+OGReaperx@users.noreply.github.com>
2024-05-20 10:47:44 +1200

    Update server.lua

--- config/server.lua
+++ config/server.lua
@@ -1,5 +1,6 @@
 return {
     stress = {
         disableForLEO = true, -- If true, it will disable stress for people in the leo job type
+        disableStress = true, -- If true, it will disable stress for everyone
     },
 }
