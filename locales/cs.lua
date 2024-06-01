f1320b01e44da7c034e4f21f91925a2d067c0941
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-06-01 12:02:05 -0400

    Update cs.lua

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,42 +1,33 @@
-local Translations = {
-    notify = {
-        hud_settings_loaded = "Nastavení HUD načteno!",
-        hud_restart = "HUD se restartuje!",
-        hud_start = "HUD je nyní spuštěn!",
-        hud_command_info = "Tento příkaz resetuje vaše aktuální nastavení HUD!",
-        load_square_map ="Načítá se mapa čtverce...",
-        loaded_square_map = "Čtvercová mapa se načetla!",
-        load_circle_map = "Načítá se mapa kruhů...",
-        loaded_circle_map = "Kruhová mapa se načetla!",
-        cinematic_on = "Cinematic Mode On!",
-        cinematic_off = "Cinematic Mode Off!",
-        engine_on = "Engine Started!",
-        engine_off = "Engine Shut Down!",
-        low_fuel = "Nízká hladina paliva!",
-        access_denied = "Nejste oprávněn!",
-        stress_gain = "Cítíte se více ve stresu!",
-        stress_removed = "Cítím se uvolněněji!",
+{
+    "notify": {
+        "hud_settings_loaded": "Nastavení HUD načteno!",
+        "hud_restart": "HUD se restartuje!",
+        "hud_start": "HUD je nyní zapnutý!",
+        "hud_command_info": "Tento příkaz resetuje vaše momentální nastavení HUD!",
+        "load_square_map": "Načítání čtvercové mapy...",
+        "loaded_square_map": "Čtvercová mapa byla načtena!",
+        "load_circle_map": "Kruhová mapa se načítá...",
+        "loaded_circle_map": "Kruhová mapa byla načtena!",
+        "cinematic_on": "Cinematic Mode On!",
+        "cinematic_off": "Cinematic Mode Off!",
+        "engine_on": "Motor nastartován!",
+        "engine_off": "Motor vypnut!",
+        "low_fuel": "Nedostatek paliva!",
+        "access_denied": "Na toto nemáš oprávnění!",
+        "stress_gain": "Cítíš se být vystresovaná!",
+        "stress_removed": "Cítíš se volněj!"
+    }
+    "info": {
+        "open_menu": "Otevřít nabídku"
     },
-    info = {
-        open_menu = "Otevřít nabídku",
-    },
-    commands = {
-        cash = 'cash',
-        bank = 'bank',
-        dev = 'dev',
-        help = {
-            cash = 'Check Cash Balance',
-            bank = 'Check Bank Balance',
-            dev = 'Zapnout/vypnout režim pro vývojáře'
+    "commands": {
+        "cash": "cash",
+        "bank": "bank",
+        "dev": "dev",
+        "help": {
+            "cash": "Check Cash Balance",
+            "bank": "Check Bank Balance",
+            "dev": "Zapnout/vypnout režim pro vývojáře"
         }
     }
 }
-
-if GetConvar('qb_locale', 'en') == 'cs' then
-    Lang = Locale:new({
-        phrases = Translations,
-        warnOnMissing = true,
-        fallbackLang = Lang,
-    })
-end
---translate by stepan_valic
